d3c2eca94e723c985342a1b4e0f0775ef0cb34b4
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-02 09:59:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word(string)
+begin
   newString = string.split
   reverseThis = newString.reverse
   finalRev = reverseThis.join(" ")
   p finalRev.reverse
+end
   
   anotherSet = string.collect
   p anotherSet.reverse
